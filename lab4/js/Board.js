e5beebb584bdb911771b9d180607af17ea5dad34
franciscomatos <francisco.a.c.matos@tecnico.ulisboa.pt>
2018-11-15 11:23:08 +0000

    Chess texture added

--- lab4/js/Board.js
+++ lab4/js/Board.js
@@ -1,5 +1,13 @@
 class Board extends THREE.Object3D {
     constructor(scene) {
         super();
+        this.createTexture();
+    }
+
+    createTexture() {
+        this.texture = THREE.TextureLoader('../textures/chessBoardTexture.png');
+        //this.texture.wrapS = this.texture.wrapT = THREE.RepeatWrappings();
+        //this.texture.repeat.set(N,N);
+        //material
     }
 }
--- lab4/js/Game.js
+++ lab4/js/Game.js
@@ -3,6 +3,7 @@ class Game {
         this.scene = new Scene(window.innerWidth, window.innerHeight);
         this.camera = new PerspectiveCamera(10, 10, 0, 0, 0, 0);
         this.controls = new THREE.OrbitControls(this.camera);
+        this.chessBoard = new Board(this);
     }
 
     getScene() {
