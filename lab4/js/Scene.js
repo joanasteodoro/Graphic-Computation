465b45360bc206fee3df31b48f1c1d2527bf6db3
joanasteodoro <joanasilvateodoro@gmail.com>
2018-11-20 14:05:32 +0000

    final

--- lab4/js/Scene.js
+++ lab4/js/Scene.js
@@ -5,35 +5,4 @@ class Scene extends THREE.Scene {
         this.add(new THREE.AxisHelper(50));
     }
 
-    getUpRotationFlag() {
-        return this.rotationFlags.up;
-    }
-
-    getDownRotationFlag() {
-        return this.rotationFlags.down;
-    }
-
-    getLeftRotationFlag() {
-        return this.rotationFlags.left;
-    }
-
-    getRightRotationFlag() {
-        return this.rotationFlags.right;
-    }
-
-    setUpRotationFlag(b) {
-        this.rotationFlags.up = b;
-    }
-
-    setDownRotationFlag(b) {
-        this.rotationFlags.down = b;
-    }
-
-    setLeftRotationFlag(b) {
-        this.rotationFlags.left = b;
-    }
-
-    setRightRotationFlag(b) {
-        this.rotationFlags.right = b;
-    }
 }
